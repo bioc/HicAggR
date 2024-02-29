57383ce88e874e20dcdd5ea3b17643f3567774f3
RobTesf <rob.tesf@gmail.com>
2024-02-29 14:56:54 +0100

    just commit 2

--- R/zzz.R
+++ R/zzz.R
@@ -4,20 +4,25 @@ hicaggrStartupMessage <- function()
 # > figlet HicAggR
   msg <- c(paste0(
 "    _/    _/  _/              _/_/                        _/_/_/    
-    _/    _/        _/_/_/  _/    _/    _/_/_/    _/_/_/  _/    _/   
-   _/_/_/_/  _/  _/        _/_/_/_/  _/    _/  _/    _/  _/_/_/      
-  _/    _/  _/  _/        _/    _/  _/    _/  _/    _/  _/    _/     
- _/    _/  _/    _/_/_/  _/    _/    _/_/_/    _/_/_/  _/    _/      
-                                        _/        _/                 
-                                   _/_/      _/_/                    ",
+   _/    _/        _/_/_/  _/    _/    _/_/_/    _/_/_/  _/    _/   
+  _/_/_/_/  _/  _/        _/_/_/_/  _/    _/  _/    _/  _/_/_/      
+ _/    _/  _/  _/        _/    _/  _/    _/  _/    _/  _/    _/     
+_/    _/  _/    _/_/_/  _/    _/    _/_/_/    _/_/_/  _/    _/      
+                                       _/        _/                 
+                                  _/_/      _/_/                    ",
 ".......version ",
 packageVersion("HicAggR")),
-"\nType 'citation(\"HicAggR\")' for citing this R package in publications.")
+"\nType 'citation(\"HicAggR\")' for citing this R package in publications.",
+"Imported---------------------",
+paste0("InteractionSet--------------",
+ifelse("package:InteractionSet"%in%search(),"\u{2713}","\u{0058}"))
+)
   return(msg)
 }
 
 .onAttach <- function(lib, pkg)
 {
+  suppressPackageStartupMessages(library("InteractionSet"))
   msg <- hicaggrStartupMessage()
   if(!interactive())
     msg[1] <- paste("Package 'HicAggR' version", packageVersion("HicAggR"))
